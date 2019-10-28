ddfd43629e2486be371c1dbba7526aae875e2e48
Muatasim Qazi <muatasimqazi@gmail.com>
2019-10-28 02:12:12 -0700

    Updates

---
+++ precache-manifest.954be9253302a92947204e96b0faa975.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "248ead4e85c0bcca1991d65d0b1ae315",
+    "url": "/amazon-web-dev/index.html"
+  },
+  {
+    "revision": "6e3bc92bde9de4fa9096",
+    "url": "/amazon-web-dev/static/css/main.b36e69c6.chunk.css"
+  },
+  {
+    "revision": "a1440fd4727668fef6c1",
+    "url": "/amazon-web-dev/static/js/2.f20b544e.chunk.js"
+  },
+  {
+    "revision": "6e3bc92bde9de4fa9096",
+    "url": "/amazon-web-dev/static/js/main.f3bfbdbc.chunk.js"
+  },
+  {
+    "revision": "33d6e1abb85dec24198d",
+    "url": "/amazon-web-dev/static/js/runtime-main.69a9bad2.js"
+  }
+]);
